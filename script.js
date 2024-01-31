258dbc486b303471b1d386bdf1d3c8230214e8be
Sstefaniee <158246961+Sstefaniee@users.noreply.github.com>
2024-01-30 23:22:17 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,103 @@
+src="jquery-3.7.1.js"
+$(document).ready(function() 
+{
+  $('#image1').hover(function ()
+{
+$('#image1').attr('src', 'images/hhmc.PNG') ;
+},
+function()//on out
+{
+  $('#image1').attr('src', 'images/Group.JPG');
+
+
+})
+
+});
+
+$(document).ready(function() 
+{
+$('#image2').hover(function ()
+{
+$('#image2').attr('src', 'images/HHMS.PNG') ;
+},
+function()//on out
+{
+  $('#image2').attr('src', 'images/hhm.PNG');
+
+})
+
+$(document).ready(function() 
+{
+$('#image3').hover(function ()
+{
+$('#image3').attr('src', 'images/fgf.PNG') ;
+},
+function()//on out
+{
+  $('#image3').attr('src', 'images/fund.PNG');
+
+  
+
+})
+$(document).ready(function() 
+{
+$('#image4').hover(function ()
+{
+$('#image4').attr('src', 'images/cr1.PNG') ;
+},
+function()//on out
+{
+  $('#image4').attr('src', 'images/clubrush1.PNG');
+
+
+  $(document).ready(function() 
+{
+$('#image5').hover(function ()
+{
+$('#image5').attr('src', 'images/cr2.PNG') ;
+},
+function()//on out
+{
+  $('#image5').attr('src', 'images/clubrush2.PNG');
+
+})
+
+
+$(document).ready(function() 
+{
+$('#image6').hover(function ()
+{
+$('#image6').attr('src', 'images/2022.PNG') ;
+},
+function()//on out
+{
+  $('#image6').attr('src', 'images/2022sale.PNG');
+
+})
+
+
+
+
+
+
+
+$(document).ready(function() 
+{
+$('#image7').hover(function ()
+{
+$('#image7').attr('src', 'images/2023.PNG') ;
+},
+function()//on out
+{
+  $('#image7').attr('src', 'images/seniorawards.PNG');
+
+})
+
+})
+})
+})
+})
+})
+})
+});
+
